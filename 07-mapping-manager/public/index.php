d39dfc7669268e0735d0cd488e214e2f8e947642
JonRauzy <jonathan.rauzy@gmail.com>
2023-05-25 12:17:23 +0200

    oui

--- 07-mapping-manager/public/index.php
+++ 07-mapping-manager/public/index.php
@@ -3,3 +3,9 @@
 session_start();
 
 require_once "../config/config.php";
+
+spl_autoload_register(function($classname){
+    $file = '../model/' . str_replace('\\', $classname) . 'php';
+    require_once $file;
+});
+
