ba8dc65118c374c4066166f36ef84650f4904ad4
JonRauzy <jonathan.rauzy@gmail.com>
2023-05-27 05:57:36 +0200

    fix

--- 07-mapping-manager/public/index.php
+++ 07-mapping-manager/public/index.php
@@ -5,7 +5,10 @@ session_start();
 require_once "../config/config.php";
 
 spl_autoload_register(function($classname){
-    $file = '../model/' . str_replace('\\', $classname) . 'php';
-    require_once $file;
+    $file = '../model/' . str_replace('\\', '/' , $classname) . 'php';
+    if(file_exists($file)){
+
+        require_once $file;
+    }
 });
 
